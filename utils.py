322b0d69ed4b479c7425d73324d77f933e9fa030
veronikasnw <veronika.bliznashka@gmail.com>
2024-05-15 12:20:53 +0200

    screen text changes

--- utils.py
+++ utils.py
@@ -81,11 +81,12 @@ def draw_styled_landmarks(image, results):
 
 
 def prob_viz(res, actions, input_frame, threshold):
-    one_color = (245,117,16)
-    output_frame = input_frame.copy()
-    for num, prob in enumerate(res):
-        color = one_color if prob < threshold else (0,0,255)
-        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), color, -1) #colors[num%len(colors)]
-        cv2.putText(output_frame, str(int(prob * 100)) + "%" + " " + actions[num] , (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+    # one_color = (245,117,16)
+    # output_frame = input_frame.copy()
+    # for num, prob in enumerate(res):
+    #     color = one_color if prob < threshold else (0,0,255)
+    #     cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), color, -1) #colors[num%len(colors)]
+    #     cv2.putText(output_frame, str(int(prob * 100)) + "%" + " " + actions[num] , (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255,255,255), 2, cv2.LINE_AA)
         
-    return output_frame
+    # return output_frame
+    pass
